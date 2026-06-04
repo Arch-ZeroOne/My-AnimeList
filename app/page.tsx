d92d3575c783ce87d64b2119d743871d1b18740f
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-06-04 21:43:13 +0800

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,81 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div>
+      <div className="max-lg:collapse bg-base-200 lg:mb-48 shadow-sm w-full rounded-md">
+        <input id="navbar-1-toggle" className="peer hidden" type="checkbox" />
+        <label
+          htmlFor="navbar-1-toggle"
+          className="fixed inset-0 hidden max-lg:peer-checked:block"
+        ></label>
+        <div className="collapse-title navbar">
+          <div className="navbar-start">
+            <label
+              htmlFor="navbar-1-toggle"
+              className="btn btn-ghost lg:hidden"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <button className="btn btn-ghost text-xl">daisyUI</button>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <button>Home</button>
+              </li>
+
+              <li>
+                <button>Categories</button>
+              </li>
+              <li>
+                <button>Favourites</button>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-64 lg:w-auto"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
         </div>
-      </main>
+
+        <div className="collapse-content lg:hidden z-1">
+          <ul className="menu">
+            <li>
+              <button>Item 1</button>
+            </li>
+            <li>
+              <button>Parent</button>
+              <ul>
+                <li>
+                  <button>Submenu 1</button>
+                </li>
+                <li>
+                  <button>Submenu 2</button>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <button>Item 3</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
