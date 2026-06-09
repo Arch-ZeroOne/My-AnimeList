8a3b2eca7563591c84159ae59494759142400012
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-06-10 00:21:34 +0800

    Solved the carousel issue

--- app/components/carousel/animecarousel.tsx
+++ app/components/carousel/animecarousel.tsx
@@ -1,52 +1,53 @@
-type AnimeInfo = {
+interface Entry {
+  images: {
+    jpg: {
+      image_url: string;
+      small_image_url: string;
+      large_image_url: string;
+    };
+  };
   title: string;
-  picture_url: string;
-  rank: number;
+  url: string;
+}
+type AnimeInfo = {
+  mal_id: number;
+  date: string;
+  entry: Entry[];
 };
 const AnimeCarousel = async () => {
   const response = await fetch(
-    "https://myanimelist.p.rapidapi.com/anime/top/all?p=1",
-    {
-      headers: {
-        "x-rapidapi-key": "b87e99e919msh6176ee1540351d0p17b2b8jsn6ad19e3992f3",
-        "x-rapidapi-host": "myanimelist.p.rapidapi.com",
-        "Content-Type": "application/json",
-      },
-    },
+    "https://api.jikan.moe/v4/recommendations/anime",
   );
 
   if (!response.ok) {
     throw new Error("Failure in fetching data");
   }
-  const result = await response.json();
-  const anime = result.slice(0, 10);
+  const { data } = await response.json();
 
-  // const handleNext = () => {
-  //   setCurrentIndex((previous) =>
-  //     previous === "#10" ? `#${10}` : `${previous + 1}`,
-  //   );
-  // };
-
-  // const handlePrevious = () => {
-  //   setCurrentIndex((previous) =>
-  //     previous === "#10" ? `#${10}` : `${previous - 1}`,
-  //   );
-  // };
+  const anime = data.slice(0, 10);
+  console.log(anime[0].entry[1].images);
 
   return (
     <div className="carousel w-md">
       {anime.map((data: AnimeInfo, index: number) => {
-        const next = index === anime.length ? 0 : index + 1;
-        const prev = index === anime.length ? anime.length - 1 : index - 1;
+        const next = index === anime.length - 1 ? 0 : index + 1;
+        const prev = index === 0 ? anime.length - 1 : index - 1;
+        const { image_url } = data.entry[1].images.jpg;
+        console.log(next);
+        console.log(prev);
 
         return (
-          <div id={String(data.rank)} className="carousel-item relative w-full">
-            <img src={data.picture_url} className="w-full" />
+          <div
+            key={index}
+            id={`slide${index}`}
+            className="carousel-item relative w-full"
+          >
+            <img src={image_url} className="w-full" />
             <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a href={`slide#${prev}`} className="btn btn-circle">
+              <a href={`#slide${prev}`} className="btn btn-circle">
                 ❮
               </a>
-              <a href={`slide#${next}`} className="btn btn-circle">
+              <a href={`#slide${next}`} className="btn btn-circle">
                 ❯
               </a>
               {}
