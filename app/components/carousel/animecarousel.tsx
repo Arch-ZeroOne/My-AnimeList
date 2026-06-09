142f226f6c24214a5614ff8947226fc4eb9a1eb6
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-06-09 23:45:56 +0800

    updated carousel mechanism

--- app/components/carousel/animecarousel.tsx
+++ app/components/carousel/animecarousel.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
 type AnimeInfo = {
   title: string;
   picture_url: string;
   rank: number;
 };
 const AnimeCarousel = async () => {
-  const [rank, setRank] = useState();
   const response = await fetch(
     "https://myanimelist.p.rapidapi.com/anime/top/all?p=1",
     {
@@ -22,32 +20,41 @@ const AnimeCarousel = async () => {
   }
   const result = await response.json();
   const anime = result.slice(0, 10);
+
+  // const handleNext = () => {
+  //   setCurrentIndex((previous) =>
+  //     previous === "#10" ? `#${10}` : `${previous + 1}`,
+  //   );
+  // };
+
+  // const handlePrevious = () => {
+  //   setCurrentIndex((previous) =>
+  //     previous === "#10" ? `#${10}` : `${previous - 1}`,
+  //   );
+  // };
+
   return (
     <div className="carousel w-md">
-      {anime.map((data: AnimeInfo) => (
-        <div id={String(data.rank)} className="carousel-item relative w-full">
-          <img src={data.picture_url} className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href={`#${10}`} className="btn btn-circle">
-              ❮
-            </a>
-            <a
-              onClick={}
-              href={`#${data.rank + 1}`}
-              className="btn btn-circle"
-            >
-              ❯
-            </a>
-            {}
+      {anime.map((data: AnimeInfo, index: number) => {
+        const next = index === anime.length ? 0 : index + 1;
+        const prev = index === anime.length ? anime.length - 1 : index - 1;
+
+        return (
+          <div id={String(data.rank)} className="carousel-item relative w-full">
+            <img src={data.picture_url} className="w-full" />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href={`slide#${prev}`} className="btn btn-circle">
+                ❮
+              </a>
+              <a href={`slide#${next}`} className="btn btn-circle">
+                ❯
+              </a>
+              {}
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
-    // <div className="flex flex-col items-center">
-    //   <div className="carousel w-[80%]">
-    //
-    //   </div>
-    // </div>
   );
 };
 
